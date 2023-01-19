af0f755af9b172379871ab87ee973abde2781e79
soca-git <soca.mailbox@gmail.com>
2023-01-19 13:50:36 +0000

    create project, add some textures

---
+++ src/Tanks/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Tanks.TanksGame();
+game.Run();
---
+++ src/Tanks/TanksGame.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Tanks
+{
+    public class TanksGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private const int _screenWidth = 800;
+        private const int _screenHeight = 600;
+
+        private Rectangle _backgroundSize = new Rectangle(0, 0, _screenWidth, _screenHeight);
+
+        Texture2D Tank;
+        Texture2D Background;
+
+        public TanksGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = _screenWidth;
+            _graphics.PreferredBackBufferHeight = _screenHeight;
+
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            Tank = Content.Load<Texture2D>("Pz.Kpfw.IV-G_preview");
+            Background = Content.Load<Texture2D>("background");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here. The order of drawing matters!
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(Background, _backgroundSize, Color.White);
+            _spriteBatch.Draw(Tank, new Rectangle(200, 50, 50, 100), Color.White);
+            // Color tints can be applied when drawing the sprites. So we could have a red tank, a green tank, etc.
+            // White will not apply any tint.
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
