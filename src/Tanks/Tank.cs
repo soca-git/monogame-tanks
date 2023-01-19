7b3e62e3a2fe02132d474ed8fbb74cd8c9266def
soca-git <soca.mailbox@gmail.com>
2023-01-19 14:25:51 +0000

    add Tank class

---
+++ src/Tanks/Tank.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Tanks
+{
+    internal class Tank
+    {
+        private readonly Texture2D _texture;
+        private readonly Rectangle _rectangle;
+        private readonly Color _color = Color.White;
+        private int x = 50;
+        private int y = 50;
+
+        public Tank(int width, int height, Texture2D texture)
+        {
+            _texture = texture;
+            _rectangle = new Rectangle(x, y, width, height);
+        }
+
+        public Tank(int width, int height, Texture2D texture, Color color)
+        {
+            _texture = texture;
+            _rectangle = new Rectangle(x, y, width, height);
+            _color = color;
+        }
+
+        public Rectangle Box => _rectangle;
+
+        public Texture2D Texture => _texture;
+
+        public Color Color => _color;
+    }
+}
--- src/Tanks/TanksGame.cs
+++ src/Tanks/TanksGame.cs
@@ -14,9 +14,9 @@ namespace Tanks
 
         private Rectangle _backgroundSize = new Rectangle(0, 0, _screenWidth, _screenHeight);
 
-        Texture2D Tank;
         Texture2D Background;
         SpriteFont Font;
+        Tank Tank;
 
         public TanksGame()
         {
@@ -40,15 +40,16 @@ namespace Tanks
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            Tank = Content.Load<Texture2D>("Pz.Kpfw.IV-G_preview");
             Background = Content.Load<Texture2D>("background");
             Font = Content.Load<SpriteFont>("defaultFont");
+            Tank = new Tank(50, 100, Content.Load<Texture2D>("Pz.Kpfw.IV-G_preview"));
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
 
             // TODO: Add your update logic here
 
@@ -57,15 +58,14 @@ namespace Tanks
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here. The order of drawing matters!
             _spriteBatch.Begin();
-            _spriteBatch.Draw(Background, _backgroundSize, Color.White);
-            _spriteBatch.Draw(Tank, new Rectangle(200, 50, 50, 100), Color.White);
-            // Color tints can be applied when drawing the sprites. So we could have a red tank, a green tank, etc.
-            // White will not apply any tint.
 
+            // Draw
+            _spriteBatch.Draw(Background, _backgroundSize, Color.White);
+            _spriteBatch.Draw(Tank.Texture, Tank.Box, Tank.Color);
             _spriteBatch.DrawString(Font, "Tanks very much!", new Vector2(5, _screenHeight-20), Color.Aquamarine);
 
             _spriteBatch.End();
