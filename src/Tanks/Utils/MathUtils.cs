41c74d0802f38c0d38efc16d3a18914f1deff2e8
soca-git <soca.mailbox@gmail.com>
2023-01-20 16:58:53 +0000

    add more math utils

--- src/Tanks/Utils/MathUtils.cs
+++ src/Tanks/Utils/MathUtils.cs
@@ -5,9 +5,20 @@ namespace Tanks.Utils
 {
     internal static class MathUtils
     {
+        public static float ToRadians(this float degrees)
+        {
+            return (float)Math.PI / 180 * degrees;
+        }
+
         public static float ToAngle(this Vector2 vector)
         {
             return (float)Math.Atan2(vector.Y, vector.X);
+        }
+
+        public static Vector2 ToVector2(this float degrees)
+        {
+            var radians = degrees.ToRadians();
+            return new Vector2((float)Math.Cos(radians), (float)Math.Sin(radians));
         }
     }
 }
