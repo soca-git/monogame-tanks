9c4bd6ca42e94a329b50a1846e61c77ae6c368b9
soca-git <soca.mailbox@gmail.com>
2023-01-20 16:12:12 +0000

    add marker for debugging

---
+++ src/Tanks/Sprites/Marker.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Tanks.Sprites
+{
+    internal class Marker : Sprite
+    {
+        public Marker(Texture2D texture, float startX, float startY, float scale, Color color)
+            : base(texture, startX, startY, scale, color)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
