f8d335d58ca065e1fa7f3fffa49cfeac1ee04014
soca-git <soca.mailbox@gmail.com>
2023-01-20 16:13:46 +0000

    add math extensions for later

---
+++ src/Tanks/Utils/MathUtils.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace Tanks.Utils
+{
+    internal static class MathUtils
+    {
+        public static float ToAngle(this Vector2 vector)
+        {
+            return (float)Math.Atan2(vector.Y, vector.X);
+        }
+    }
+}
